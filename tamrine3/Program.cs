ff5d1de898158931d10e788cfb76c7c6218c6b09
fateme jouyande <fj.1403@outlook.com>
2018-03-09 16:20:17 +0330

    initial commit

---
+++ tamrine3/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tamrine3
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            int[] intArr = new int[8];
+            int input = 0;
+            double probability;
+            for (int i = 0; i < 8; i++)
+            {
+                while (input <= 0)
+                {
+                    Console.WriteLine("Onsore " + i.ToString() + " om arraye ra vared nemayeed:");
+                    Int32.TryParse(Console.ReadLine(), out input);
+                    if (input > 0)
+                    {
+                        intArr[i] = input;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Adade vared shode motabaer nist. Lotfan mojadadan talash konid.");
+                    }
+                }
+                input = 0;
+            }
+
+            input = 0;
+            while (input != -1)
+            {
+                Console.WriteLine("Ehtemale entekhabe kodam adad ra mikhahid:");
+                Int32.TryParse(Console.ReadLine(), out input);
+                probability = calculateProbability(intArr, input);
+
+                Console.WriteLine("Ehtemale entekhabe adade " + input + ", " + probability + " ast.");
+            }
+
+        }
+        public static double calculateProbability(int[] arr, int number)
+        {
+            int counter = 0;
+            foreach (var item in arr)
+            {
+                if (item == number)
+                {
+                    counter += 1;
+                }
+            }
+            return Convert.ToDouble(counter) / Convert.ToDouble(arr.Length);
+        }
+    }
+}
